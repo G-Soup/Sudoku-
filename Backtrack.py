d26bde27afed369a9d201c858ce71fcdcdac81e4
G-Soup <grhmcmpbll@gmail.com>
2020-03-03 19:18:14 -0500

    Made an additional solution that uses recursion to plug in numbers to test almost every possibility

---
+++ Backtrack.py
@@ -0,0 +1,49 @@
+
+
+
+def print_board(board):
+    for x in range(9):
+        print(board[x])
+        print('\n')
+
+def find_empty(board):
+    for x in range(9):
+        for y in range(9):
+            if board[x][y] == '.':
+                return [x,y]
+
+def guess_solve(board):
+    empty = find_empty(board)
+    if not empty:
+        return True
+    else:
+        row = empty[0]
+        column = empty[1]
+    
+    for x in range(1,10):
+        if is_valid(board, str(x), [row, column]):
+            board[row][column] = str(x)
+
+            if guess_solve(board):
+                return True
+            board[row][column] = '.'
+    return False
+
+def is_valid(board, number, location):
+
+    for x in range(9):
+        if board[location[0]][x] == number and location[1] != x:
+            return False
+    
+    for x in range(9):
+        if board[x][location[1]] == number and location[0] != x:
+            return False
+    
+    local_location = [int(location[0]/3)*3, int(location[1]/3)*3]
+
+    for x in range(local_location[0], local_location[0]+3):
+        for y in range(local_location[1], local_location[1]+3):
+            if board[x][y] == number and [x,y] != location:
+                return False
+    
+    return True
